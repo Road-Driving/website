7a234e46bae8e31f5e0ea44a361273b7f9c04099
evepoi <evepoi@naver.com>
2026-05-08 15:24:38 +0900

    Update home page

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,23 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <HelloWorld />
+  <div class="container">
+    <h1>Road Driving</h1>
+    <p>버스기사의 기록 공간</p>
+  </div>
 </template>
+
+<style>
+body {
+  margin: 0;
+  background: #0f172a;
+  color: white;
+  font-family: sans-serif;
+}
+
+.container {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
