56086443be3a6d4ffe6f4e7c540f1dcc360f48f8
evepoi <evepoi@naver.com>
2026-05-08 16:16:42 +0900

    Fix Vite base config

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,6 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig(({ command }) => {
   return {
     plugins: [vue()],
-
-    // 로컬 개발 시
-    // npm run dev
-    base: command === 'serve'
-      ? '/'
-      : '/website/',
+    base: command === 'serve' ? '/' : '/website/',
   }
 })
